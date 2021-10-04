dd786c9f58425decd68b2d20e8118e46ecc4e9c5
zhigarevyan <zhigarevyan@gmail.com>
2021-10-04 17:11:14 +0300

    cred

--- controller/src/main/java/com/epam/esm/configuration/SpringBootStarter.java
+++ controller/src/main/java/com/epam/esm/configuration/SpringBootStarter.java
@@ -2,10 +2,11 @@ package com.epam.esm.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class SpringBootStarter {
+public class SpringBootStarter extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootStarter.class, args);
